7fa4636e2cf364d4cb356f8dc0d5530d1c0d26b3
Saurav <sauravbarnwal9@gmail.com>
2022-09-28 19:50:48 +0530

    Updates

---
+++ precache-manifest.29d120e2d7a377db16ffe5000000bd38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b93965ec95bc02d158d94e8cd64562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9c7a25a59dd2bdbf51",
+    "url": "/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "5e44d9f81821ca3ab504",
+    "url": "/static/css/main.682712f7.chunk.css"
+  },
+  {
+    "revision": "5d9c7a25a59dd2bdbf51",
+    "url": "/static/js/2.d35f182f.chunk.js"
+  },
+  {
+    "revision": "590405f2965d46ad4f9da020f83d4569",
+    "url": "/static/js/2.d35f182f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e44d9f81821ca3ab504",
+    "url": "/static/js/main.57344e1c.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "bca6cb728077b14f91b236dea1c197fa",
+    "url": "/static/media/Saurav_Barnwal_Resume.bca6cb72.pdf"
+  }
+]);
